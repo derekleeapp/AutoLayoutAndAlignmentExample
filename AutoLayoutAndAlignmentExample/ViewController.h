5be15d72e7bff0eaf75716eb4913408609a0341f
Derek Lee <dereklee@downtothis.com>
2014-10-22 23:54:14 +0900

    Initial commit.

---
+++ AutoLayoutAndAlignmentExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AutoLayoutAndAlignmentExample
+//
+//  Created by Derek Lee on 10/22/14.
+//  Copyright (c) 2014 Derek Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ AutoLayoutAndAlignmentExample/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AutoLayoutAndAlignmentExample
+//
+//  Created by Derek Lee on 10/22/14.
+//  Copyright (c) 2014 Derek Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
